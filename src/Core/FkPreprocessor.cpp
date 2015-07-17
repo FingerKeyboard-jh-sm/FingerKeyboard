3e35c5c87b922be5fc9c1c62e12fd635d95f5f17
jhKang1313 <jinhuk1313@gmail.com>
2015-07-18 04:17:15 +0900

    testest

--- src/Core/FkPreprocessor.cpp
+++ src/Core/FkPreprocessor.cpp
@@ -1,5 +1,6 @@
 #include"FkPreProcessor.h"
 //process is to deal with the collected data at program
+//...
 FkPreprocessor::FkPreprocessor(int keyboardType):paperKeyboardRecognizer(keyboardType){
 
 }
