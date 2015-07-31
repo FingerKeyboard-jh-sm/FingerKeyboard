6e6373374c05a2cf445ce48cdde9eb4c0d2b682c
jhKang1313 <jinhuk1313@gmail.com>
2015-08-01 02:19:18 +0900

    키 이벤트 처리를 위한 Message Queue Thread 작성.

---
+++ src/Core/FkEventMessage.h
@@ -0,0 +1,14 @@
+#ifndef FK_EVENT_MSG_
+#define FK_EVENT_MSG_
+class FkEventMessage{
+	
+
+
+
+
+
+
+
+
+};
+#endif
---
+++ src/Core/FkVirtualKeyEventListener.h
@@ -0,0 +1,16 @@
+#ifndef FK_KEY_EVENT
+#define FK_KEY_EVENT
+#include"FkThread.h"
+#include"FkKey.h"
+#include"FkCondition.h"
+#include"FkEventMessage.h"
+class FkVirtualKeyEventListener : public FkThread{
+	
+
+public:
+	FkVirtualKeyEventListener();
+	
+};
+
+
+#endif
